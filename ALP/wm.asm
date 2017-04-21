21d64a12f2aa6491a3216debd261f9e40805c6f2
Rajat Jain <1997.rajatjain@gmail.com>
2017-04-21 21:17:16 +0530

    Modes Update
    Added
    - Fully functional 7-seg display
    Removed
    - LED Display

--- ALP/wm.asm
+++ ALP/wm.asm
@@ -12,21 +12,23 @@
     CREG1_8259 EQU 12H
     MODENO DB 00H
     STACK DW 100 DUP(?)
-    ;TOP_STACK REMEMBER TO DEFINE!!!    
+    TOP_STACK LABEL WORD   
 .code
 .startup
     LEA SP, TOP_STACK        
     MOV AL,10010000B
-    OUT CREG_8255,AL
+    OUT CREG_8255,AL  
+    
+    
     
     START:
         IN AL, PORTA
-        CMP AL, 11101110B
+        CMP AL, 11111110B
     JNZ START  
     CALL DEBOUNCE_DELAY
-    MOV AL,00001111B
-    OUT CREG_8255,AL      
-          
+    ;MOV AL,00001111B
+    ;OUT CREG_8255,AL      
+    
     LOAD:
         IN AL, PORTA
         CMP AL, 11101111B
@@ -37,17 +39,14 @@
         CALL DEBOUNCE_DELAY
     JMP LOAD
     LOADEXIT:
-    CALL DEBOUNCE_DELAY
-    MOV AL, 09H
-    OUT PORTC, AL
-    ;MOV AH, MODENO
-    ;MOV BL, 00H
-    ;MOV MODENO, BL
-    ;CMP AH, 00H
-    ;JZ LOAD
-    ;CMP AH, 03H
-    ;JG LOAD
-    ;MOV MODENO, AH        
+    MOV AH, MODENO
+    MOV BL, 00H
+    MOV MODENO, BL
+    CMP AH, 00H
+    JZ LOAD
+    CMP AH, 03H
+    JG LOAD
+    MOV MODENO, AH        
     
     OUT1: 
     CMP AH, 01H
@@ -56,7 +55,18 @@
     OUT PORTC, AL
     JMP LIGHT
     OUT2:
-    LIGHT:        
+    CMP AH, 02H
+    JNE OUT3
+    MOV AL, 02H
+    OUT PORTC, AL
+    JMP MEDIUM
+    OUT3:
+    MOV AL, 03H
+    OUT PORTC, AL
+    JMP HEAVY
+    LIGHT:
+    MEDIUM:
+    HEAVY:        
     
     INF:
     JMP INF
